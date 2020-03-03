bf1d33c7c850a9ef7b577b3901a0c8ccc6b82ec2
dovahlana <atennison25@gmail.com>
2020-03-03 15:28:33 -0600

    sendgrid running

--- server/index.js
+++ server/index.js
@@ -52,6 +52,18 @@ app.get('/api/suggestions', getSuggestions);
 //traits
 // app.get('/api/traits', getTraits)
 
+//sendgrid test
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
 
 
 app.listen(SERVER_PORT, () => console.log(`Howdy pardner, server is runnin' on ${SERVER_PORT}!`))
